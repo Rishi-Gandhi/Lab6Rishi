362832c1087fc18c5c30d2999e6cb8cb069a3da6
rishigandhi <rgtiger9@gmail.com>
2024-10-20 19:12:44 -0400

    added main method and encode function

---
+++ password.py
@@ -0,0 +1,34 @@
+
+def encode(password):
+    encoded_password = ""
+    for element in password:
+        # Convert the digit character to an integer
+        digit = (int(element) + 3) % 10
+        # Append the encoded digit to the result
+        encoded_password += str(digit)
+    return encoded_password
+
+#add decoded method here:
+
+
+
+while True:
+    print("Menu")
+    print("-------------")
+    print("1. Decode")
+    print("2. Encode")
+    print("3. Quit\n")
+    menu_option = int(input("Please enter your option: "))
+
+    if menu_option == 1:
+        original_password = input("Please enter your password to encode: ")
+        encoded_password = encode(original_password)
+        print("Your password has been encoded and stored!")
+    elif menu_option == 2:
+        decoded_password = decode(encoded_password)
+        print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}")
+    elif menu_option == 3:
+        break
+
+
+
